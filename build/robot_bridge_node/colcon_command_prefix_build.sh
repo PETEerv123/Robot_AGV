ae4f53d06aa21930c0de804b5b6f2361ab833a18
JETSON <tathuan9d@gmail.com>
2026-07-25 22:51:38 +0700

    :

--- build/robot_bridge_node/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jetson/Desktop/Robot_AGV/install/serial/share/serial/package.sh"
--- build/serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/Desktop/Robot_AGV/src/serial
-# Build directory: /home/jetson/Desktop/Robot_AGV/build/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
